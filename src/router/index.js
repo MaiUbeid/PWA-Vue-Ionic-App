4f2f7c358006ecce0f1f3e3f69f8f7d2467be03c
maiubeid <ubead95@gmail.com>
2021-04-02 20:46:29 +0300

    install and update router

--- src/router/index.js
+++ src/router/index.js
@@ -1,29 +1,15 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Vue from 'vue';
+import { IonicVueRouter } from '@ionic/vue';
+import Home from '../views/Home.vue';
 
-Vue.use(VueRouter)
+Vue.use(IonicVueRouter);
 
-const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
-]
-
-const router = new VueRouter({
-  mode: 'history',
-  base: process.env.BASE_URL,
-  routes
-})
-
-export default router
+export default new IonicVueRouter({
+    mode: 'history',
+    base: process.env.BASE_URL,
+    routes: [{
+        path: '/',
+        name: 'home',
+        component: Home
+    }]
+});
